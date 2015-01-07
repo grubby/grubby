a17af6f6208e484c9730055812a27ce2301d0dec
Tim Jarratt <tjarratt@gmail.com>
2015-01-06 20:33:59 -0800

    Methods can have arguments with default values

--- interpreter/vm/builtins/class_provider.go
+++ interpreter/vm/builtins/class_provider.go
@@ -1,5 +1,13 @@
 package builtins
 
+import (
+	"github.com/grubby/grubby/ast"
+)
+
+type ArgEvaluator interface {
+	EvaluateArgInContext(ast.Node, Value) (Value, error)
+}
+
 type ClassProvider interface {
 	ClassWithName(string) Class
 }
