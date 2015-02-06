dbf79128639a314bebdf708ae169609043644a9c
Tim Jarratt <tjarratt@gmail.com>
2015-02-05 22:20:52 -0800

    Backfill a test for the callstack
    Noticed some strange behavior with the callstack where it was always gaining
    stacks but never discarding them. Thought this might be the cause of it, but it
    clearly was not.

---
+++ interpreter/vm/callstack_test.go
@@ -0,0 +1,54 @@
+package vm_test
+
+import (
+	. "github.com/grubby/grubby/interpreter/vm"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("The call stack", func() {
+	var subject *CallStack
+
+	BeforeEach(func() {
+		subject = NewCallStack()
+	})
+
+	It("initially has an empty stack", func() {
+		Expect(subject.Frames).To(BeEmpty())
+	})
+
+	Describe("adding a method to the stack", func() {
+		BeforeEach(func() {
+			subject.Unshift("some-method", "some-file")
+		})
+
+		It("should now have one item in its frames", func() {
+			Expect(len(subject.Frames)).To(Equal(1))
+			Expect(subject.Frames[0].File).To(Equal("some-file"))
+			Expect(subject.Frames[0].Method).To(Equal("some-method"))
+		})
+
+		Context("when that frame is removed", func() {
+			BeforeEach(func() {
+				subject.Shift()
+			})
+
+			It("then has no frames again", func() {
+				Expect(subject.Frames).To(BeEmpty())
+			})
+		})
+
+		Context("when more frames are added", func() {
+			BeforeEach(func() {
+				subject.Unshift("method2", "file2")
+				subject.Unshift("method3", "file3")
+			})
+
+			It("should add the last frame to the front of the stack", func() {
+				Expect(len(subject.Frames)).To(Equal(3))
+				Expect(subject.Frames[0].File).To(Equal("file3"))
+				Expect(subject.Frames[0].Method).To(Equal("method3"))
+			})
+		})
+	})
+})
