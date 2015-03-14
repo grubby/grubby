f71fc7689d185488415c5296f51fd358a6e66aea
Tim Jarratt <tjarratt@gmail.com>
2015-03-13 22:12:10 -0700

    Refactor constant lookup
    This pyramid of doom wasn't quite as stable a base as you might think. Using
    a maybe monad here feels like it makes this easier to reason about.

--- interpreter/vm/class.go
+++ interpreter/vm/class.go
@@ -2,6 +2,7 @@ package vm
 
 import (
 	"github.com/grubby/grubby/ast"
+	"github.com/tjarratt/gomads"
 
 	. "github.com/grubby/grubby/interpreter/vm/builtins"
 )
@@ -15,33 +16,51 @@ func interpretClassInContext(
 	var (
 		value Value
 		ok    bool
-		err   error
 	)
 
 	name := class.FullName()
-	value, ok = vm.CurrentClasses[name]
-	if !ok {
-		value, ok = vm.CurrentModules[name]
+	maybeTheValue := gomads.Maybe(func() interface{} {
+		return vm.CurrentClasses[name]
+	}).OrSome(gomads.Maybe(func() interface{} {
+		return vm.CurrentModules[name]
+	})).OrSome(gomads.Maybe(func() interface{} {
+		module, ok := vm.CurrentClasses[class.Namespace]
 		if !ok {
-			module, ok := vm.CurrentClasses[class.Namespace]
-			if !ok {
-				module, ok := vm.CurrentModules[class.Namespace]
-				if !ok {
-					return nil, NewNameError(name, context.String(), context.Class().String(), vm.stack.String())
-				} else {
-					value, err = module.Constant(class.Name)
-					if err != nil {
-						return nil, NewNameError(name, context.String(), context.Class().String(), vm.stack.String())
-					}
-				}
-			} else {
-				value, err = module.Constant(class.Name)
-				if err != nil {
-					return nil, NewNameError(name, context.String(), context.Class().String(), vm.stack.String())
-				}
-			}
+			return nil
 		}
-	}
 
-	return value, nil
+		value, err := module.Constant(class.Name)
+		if err != nil {
+			return nil
+		}
+
+		return value
+	})).OrSome(gomads.Maybe(func() interface{} {
+		module, ok := vm.CurrentModules[class.Namespace]
+		if !ok {
+			return nil
+		}
+
+		value, err := module.Constant(class.Name)
+		if err != nil {
+			return nil
+		}
+
+		return value
+	})).OrSome(
+		NewNameError(
+			name,
+			context.String(),
+			context.Class().String(),
+			vm.stack.String(),
+		),
+	)
+
+	value, ok = maybeTheValue.Value().(Value)
+
+	if ok {
+		return value, nil
+	} else {
+		return nil, value.(error)
+	}
 }
