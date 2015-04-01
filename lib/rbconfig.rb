31d312021083b225350b084ac391e63192337d05
Tim Jarratt <tjarratt@gmail.com>
2015-04-01 08:09:35 -0700

    Import 'rbconfig.rb' from MRI 2.1.1
    Fairly incomplete, but enough to keep going with running rubyspec.

---
+++ lib/rbconfig.rb
@@ -0,0 +1,17 @@
+
+# This file was blindly copied from MRI 2.1.1 by Tim Jarratt when grubby was built.
+# He then modified it to suit his whims and needs at the time.
+# Any inconsistencies in this file are entirely his fault.
+
+module RbConfig
+  CONFIG = {}
+  CONFIG["MAJOR"] = "2"
+  CONFIG["MINOR"] = "1"
+  CONFIG["TEENY"] = "1"
+  CONFIG["PATCHLEVEL"] = "0"
+  CONFIG["ruby_install_name"] = "grubby"
+  CONFIG["RUBY_INSTALL_NAME"] = "grubby"
+  CONFIG["RUBY_SO_NAME"] = "grubby.2.1.0"
+  CONFIG["PACKAGE"] = "ruby"
+  CONFIG["RUBY_SEARCH_PATH"] = ""
+end
